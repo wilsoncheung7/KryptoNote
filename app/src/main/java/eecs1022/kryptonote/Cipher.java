c4cda988ac38560c9d55ffc3a09d11b7d7425645
Mikan <37231830+wilsoncheung7@users.noreply.github.com>
2020-03-08 19:52:01 -0400

    second commit

--- app/src/main/java/eecs1022/kryptonote/Cipher.java
+++ app/src/main/java/eecs1022/kryptonote/Cipher.java
@@ -1,25 +1,47 @@
 package eecs1022.kryptonote;
 
 public class Cipher {
+
     private String key;
     public static final String ALPHABET="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
     public Cipher(String k){
-
+        this.key=k;
     }
 
     public String Encrypt(String note){
-        return "";
+        String pad=makePad(note);
+        String result="";
+        for(int i=0;i<note.length();i++){
+           String c=note.substring(i,i+1);
+            int position=ALPHABET.indexOf(c);
+            int shift=Integer.parseInt(pad.substring(i,i+1));
+            int newPosition=(position+shift)%ALPHABET.length();
+            result=result.substring(newPosition,newPosition+1);
+        }
+        return result;
     }
 
     public String Decrypt(String note){
-        return "";
+        String pad=makePad(note);
+        String result="";
+        for(int i=0;i<note.length();i++){
+            String c=note.substring(i,i+1);
+            int position=ALPHABET.indexOf(c);
+            int shift=Integer.parseInt(pad.substring(i,i+1));
+            int newPosition=position-shift;
+            if (newPosition<ALPHABET.length())
+                newPosition=newPosition+ALPHABET.length();
+            result=result.substring(newPosition,newPosition+1);
+        }
+        return result;
     }
 
     private String makePad(String note){
         String pad=this.key;
-        for (;pad.length()<=note.length();)
+        while (pad.length()<=note.length()){
             pad+=this.key;
+        }
         return pad;
     }
 }
--- app/src/main/java/eecs1022/kryptonote/KryptoNoteActivity.java
+++ app/src/main/java/eecs1022/kryptonote/KryptoNoteActivity.java
@@ -3,6 +3,16 @@ package eecs1022.kryptonote;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
 
 public class KryptoNoteActivity extends AppCompatActivity {
 
@@ -11,4 +21,63 @@ public class KryptoNoteActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.kryptonote_layout);
     }
+
+    public void onEncrypt(View v){
+
+        try{
+            EditText keyView=findViewById(R.id.key);
+//            String key=keyView.getText().toString();
+//            Cipher cipher=new Cipher(key);
+//            ((TextView)findViewById(R.id.data)).setText(cipher.Encrypt(key));
+        }
+
+        catch (Exception e){
+            System.out.println("Error");
+        }
+    }
+
+    public void onDecrypt (View v){
+        try{
+
+        }
+
+        catch (Exception e){
+
+        }
+    }
+
+    public void onSave(View v){
+        try{
+            String name=((EditText) findViewById(R.id.file)).getText().toString();
+            File dir=this.getFilesDir();
+            File file=new File(dir,name);
+            FileWriter fw=new FileWriter(file);
+            fw.write(((EditText)findViewById(R.id.data)).getText().toString());
+            fw.close();
+            Toast.makeText(this,"Note Saved",Toast.LENGTH_LONG);
+        }
+
+        catch (Exception e){
+            Toast.makeText(this,e.getMessage(),Toast.LENGTH_LONG);
+        }
+    }
+
+    public void onLoad(View v){
+        try{
+            String name=((EditText) findViewById(R.id.file)).getText().toString();
+            File dir=this.getFilesDir();
+            File file=new File(dir,name);
+            FileReader fr=new FileReader(file);
+            String show="";
+            for (int c=fr.read();c!=-1;c=fr.read())
+                show+=(char)c;
+            fr.close();
+            ((EditText)findViewById(R.id.data)).setText(show);
+        }
+
+        catch (Exception e){
+
+        }
+    }
+
 }
